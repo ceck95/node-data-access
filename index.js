f47f89017bfad4569f1d021c9dc3125634f6536f
nhutdev <tranvannhut4495@gmail.com>
2016-10-29 22:57:53 +0700

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: toan.nguyen
+ * @Date:   2016-09-09 12:23:34
+* @Last modified by:   nhutdev
+* @Last modified time: 2016-10-29T22:54:52+07:00
+ */
+
+'use strict';
+
+module.exports = {
+  Server: require('./lib/server'),
+};
